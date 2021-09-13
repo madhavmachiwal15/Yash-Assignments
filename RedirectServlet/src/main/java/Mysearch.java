15fc68e8e1297879597d446623ceba8bafda9a3d
madhavmachiwal15 <89064507+madhavmachiwal15@users.noreply.github.com>
2021-09-13 21:50:03 +0530

    Add files via upload

---
+++ RedirectServlet/src/main/java/Mysearch.java
@@ -0,0 +1,23 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Mysearch
+ */
+@WebServlet("Mysearch.java")
+public class Mysearch extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String str = request.getParameter("name");
+		response.sendRedirect("https://www.google.com"+str);
+	}
+
+}
