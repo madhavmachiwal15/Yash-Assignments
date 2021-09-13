3c66753450a0a2f5e3c9d61016f815b9a81bc0da
madhavmachiwal15 <89064507+madhavmachiwal15@users.noreply.github.com>
2021-09-13 21:33:35 +0530

    Add files via upload

---
+++ EmployeeInfo/src/main/java/EmployeeNextController.java
@@ -0,0 +1,32 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class EmployeeNextController
+ */
+@WebServlet("/EmployeeNextController")
+public class EmployeeNextController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String employeeName = request.getParameter("name");
+		String employeeContact = request.getParameter("contact");
+		HttpSession session = request.getSession();
+		session.setAttribute("empName", employeeName);
+		session.setAttribute("empContact", employeeContact);
+		ServletContext application = getServletContext();
+		application.getRequestDispatcher("/employee_next.html").forward(request, response);
+		
+	}
+
+}
